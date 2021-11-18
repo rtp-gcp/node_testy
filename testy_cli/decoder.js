43830c87f50e47eb6b42f10fa3772d4e26601af6
John F. Davis <davisjf@gmail.com>
2021-11-18 11:50:37 -0500

    wip

--- testy_cli/decoder.js
+++ testy_cli/decoder.js
@@ -4,21 +4,37 @@ const readline = require('readline').createInterface({
 });
 
 const pb = require('@dendra-science/goes-pseudo-binary');
- 
-readline.question('Enter input buffer ', input_buffer  => {
 
 
-   let pbf;
-   let ret;
 
-   pbf = new pb.Decoder('fp2_29, fp2_29');
-   ret = pbf.decode(${input_buffer});
-   ret.then((val) => {
-    console.log(val.rows.toString());
-   });
+// Paste this buffer for the decode operation
+// CKDS@[AKCBEO@N~@@@CQRBKEF@CAKCCET@N~@@@CQRB_DH@JAYCCEZ@N}@@@CQRB`DD@BAYCDE`@N}@@@CQRB[DK@KAICCEB@N}@@@CQRBTEF@NBOCCEE@N}@@@CQRBVCN@KBKCEEL@N~@@@CQRBFED@NAZCCEI@N}@@@CQRBFBN@IANCDEB@N~@@@CQRBTCR@KASCFED@N~@@@CQRCADL@MBMCDET@N~@@@CQRB}DW@]AICDE~@N~@@@CQRBG
 
+let input_buffer;
+readline.question('Enter input buffer ', (input_buffer) => {
 
-  readline.close();
+    let pbf;
+    let ret;
+
+    // this will dump the buffer we resoond with 
+    //console.log(input_buffer);
+
+    // this should decode it and print the result
+    pbf = new pb.Decoder('fp2_29, fp2_29');
+    ret = pbf.decode(input_buffer);
+    ret.then((val) => {
+        console.log(val.rows.toString());
+    });
+    readline.close();
 });
+
+//console.log('hey ');
+//console.log(input_buffer);
+
+
+
+
+
+//  readline.close();
 
 
