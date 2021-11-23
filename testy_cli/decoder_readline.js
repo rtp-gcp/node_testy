687d4bc7f2e947523b7b3f4755190d089da24f49
John F. Davis <davisjf@gmail.com>
2021-11-23 13:05:12 -0500

    wip

--- testy_cli/decoder_readline.js
+++ testy_cli/decoder_readline.js
@@ -5,8 +5,6 @@ const readline = require('readline').createInterface({
 
 const pb = require('@dendra-science/goes-pseudo-binary');
 
-
-
 // Paste this buffer for the decode operation
 // CKDS@[AKCBEO@N~@@@CQRBKEF@CAKCCET@N~@@@CQRB_DH@JAYCCEZ@N}@@@CQRB`DD@BAYCDE`@N}@@@CQRB[DK@KAICCEB@N}@@@CQRBTEF@NBOCCEE@N}@@@CQRBVCN@KBKCEEL@N~@@@CQRBFED@NAZCCEI@N}@@@CQRBFBN@IANCDEB@N~@@@CQRBTCR@KASCFED@N~@@@CQRCADL@MBMCDET@N~@@@CQRB}DW@]AICDE~@N~@@@CQRBG
 
@@ -21,7 +19,9 @@ readline.question('Enter input buffer ', (input_buffer) => {
     //console.log(input_buffer);
 
     // this should decode it and print the result
-    pbf = new pb.Decoder('fp2_29, fp2_29');
+    //pbf = new pb.Decoder('fp2_29, fp2_29');
+    //pbf = new pb.Decoder('fp2_6,fp2_12 ');
+    pbf = new pb.Decoder('i_2_12');
     fp2Buf = Buffer.from(input_buffer,'ascii')
     ret = pbf.decode(fp2Buf);
     ret.then((val) => {
